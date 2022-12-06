f4dbad29597cbcc9b0874de296ceefd0bf4476e9
trouw <116220815+trouw@users.noreply.github.com>
2022-12-06 17:17:23 -0500

    Update streamlit_kg.py

--- streamlit_kg.py
+++ streamlit_kg.py
@@ -7,7 +7,7 @@ st.title("Query Google's Kowledge Graph")
 container = st.container()
 
 container.write("This tool makes it easier and simplier to query Google's Knowledge Graph using Google's Knowledge Graph API.")
-container.write("Before using the tool, retrieve an API key through the Google Cloud platform. Find a quicklink in the \"?\" next to the query input box.")
+container.write("Before using the tool, retrieve an API key through the Google Cloud platform. Find a quicklink in the \"?\" next to the API Key input box.")
 
 key = container.text_input("Input API Key Below:", help="https://developers.google.com/knowledge-graph/how-tos/authorizing")
 query = container.text_input("Input Query Below")
