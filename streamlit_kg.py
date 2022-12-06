35cb34d4e826c5ad80bbbfd55012301573739293
trouw <116220815+trouw@users.noreply.github.com>
2022-12-06 17:14:26 -0500

    Update streamlit_kg.py

--- streamlit_kg.py
+++ streamlit_kg.py
@@ -2,7 +2,10 @@ import pandas as pd
 import streamlit as st
 from advertools import knowledge_graph
 
+st.title("Query Google Kowledge Graph")
+
 container = st.container()
+container.write("This tool makes it easier and simplier to query Google Knowledge GRaph using Google's Knowledge Graph API. Before using the tool, retrieve an API key through the Google Cloud platform. Find a quicklink in the "?" next to the query input box.   
 key = container.text_input("Input API Key Below:", help="https://developers.google.com/knowledge-graph/how-tos/authorizing")
 query = container.text_input("Input Query Below")
 
