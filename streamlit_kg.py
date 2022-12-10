8c87cf4220404bf770fe1bada1be52e8d6f46d32
trouw <116220815+trouw@users.noreply.github.com>
2022-12-10 15:21:10 -0500

    Update streamlit_kg.py

--- streamlit_kg.py
+++ streamlit_kg.py
@@ -12,11 +12,7 @@ container.write("Before using the tool, retrieve an API key through the Google C
 container.write("Created by [Tyler Rouwhorst](https://www.linkedin.com/in/tyler-rouwhorst/)")
 
 key = container.text_input("Input API Key Below:", help="https://developers.google.com/knowledge-graph/how-tos/authorizing")
-
-container.write("Query the Knowledge graph using a Query or Entity ID. The fields can be inputed together or one at a time")
-
 query = container.text_input("Input Query Below")
-entity_id = container.text_input("Input Entity ID", value="None")
 
 options = st.multiselect(
     'Select Elements for Export',
@@ -26,22 +22,13 @@ csv = None
 
 if st.button("Start Query"):
     try:
-        if entity_id != None: 
-            kg_df = knowledge_graph(key=key, query=query, ids=entity_id)
-            if kg_df.empty: 
-                None
-            else:
-                kg_df.rename(columns ={'query':'Query','resultScore':'Result Score','result.name':'Name','result.url':'Result URL','result.detailedDescription.url':'Detailed Description Source URL','result.detailedDescription.articleBody':'Detailed Description','result.@type':'Schema.org Type','result.image.url':'Image URL','result.description':'Description','result.@id':'Result ID'}, inplace=True)
-                user_df = kg_df[options]
-                csv = user_df.to_csv().encode('utf-8')
+        kg_df = knowledge_graph(key=key, query=query)
+        if kg_df.empty: 
+            None
         else:
-            kg_df = knowledge_graph(key=key, query=query)
-            if kg_df.empty: 
-                None
-            else:
-                kg_df.rename(columns ={'query':'Query','resultScore':'Result Score','result.name':'Name','result.url':'Result URL','result.detailedDescription.url':'Detailed Description Source URL','result.detailedDescription.articleBody':'Detailed Description','result.@type':'Schema.org Type','result.image.url':'Image URL','result.description':'Description','result.@id':'Result ID'}, inplace=True)
-                user_df = kg_df[options]
-                csv = user_df.to_csv().encode('utf-8')
+            kg_df.rename(columns ={'query':'Query','resultScore':'Result Score','result.name':'Name','result.url':'Result URL','result.detailedDescription.url':'Detailed Description Source URL','result.detailedDescription.articleBody':'Detailed Description','result.@type':'Schema.org Type','result.image.url':'Image URL','result.description':'Description','result.@id':'Result ID'}, inplace=True)
+            user_df = kg_df[options]
+            csv = user_df.to_csv().encode('utf-8')
     except:
         st.write("Sorry, there are no results for this query")
 
